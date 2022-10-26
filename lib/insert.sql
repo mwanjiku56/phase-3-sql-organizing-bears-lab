3c4cc04de4e0152995f4297ee90310a672517305
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-26 22:37:30 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,sex,age) VALUES ("Mr. Chocolate","F",2);
+INSERT INTO bears (name,sex,age) VALUES ("Rowdy","M",5);
+INSERT INTO bears (name,sex,age) VALUES ("Tabitha","F",6);
+INSERT INTO bears (name,sex,age) VALUES ("Sergeant Brown","M",3);
+INSERT INTO bears (name,sex,age) VALUES ("Melissa","F",10);
+INSERT INTO bears (name,sex,age) VALUES ("Grinch","M",1);
+INSERT INTO bears (name,sex,age) VALUES ("Wendy","F",4);
+INSERT INTO bears (name,sex,age) VALUES (NULL,"F",NULL);
